d1ebdf2760bc7c204c8b66b21b52fc48b9eb9ef5
Hasan-eltropy <hasan@eltropy.com>
2022-07-08 18:48:44 +0530

    Update main.go to add new routes

--- main.go
+++ main.go
@@ -7,10 +7,23 @@ import "os"
 func main() {
 	r := gin.Default()
 
-        host := os.Getenv("HOSTNAME")
-        reply:= fmt.Sprintf("Hello from %s V2\n", host)
+	host := os.Getenv("HOSTNAME")
+	reply:= fmt.Sprintf("Hello from %s V2\n", host)
+	appName := os.Getenv("APPNAME")
+	if appName == "" {
+		appName = "app"
+	}
+
 	r.GET("/hello", func(c *gin.Context) {
 		c.String(200, reply)
+	})
+        
+	r.GET("/"+appName+"/hello", func(c *gin.Context) {
+		c.String(200, reply)
+	})
+
+	r.GET("/"+appName+"/exit", func(c *gin.Context) {
+		os.Exit(0)
 	})
 
 	r.Run(":3000")
