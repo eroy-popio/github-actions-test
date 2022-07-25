601633f3cb6fbe5f922b944afcd21d578aef2bbb
Hasan-eltropy <hasan@eltropy.com>
2022-07-25 18:35:01 +0530

    Update version to v4

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 
 	host := os.Getenv("HOSTNAME")
-	reply:= fmt.Sprintf("Hello from %s V3 \n", host)
+	reply:= fmt.Sprintf("Hello from %s V4 \n", host)
 	newStr := NewString("Hello, World! ")
 	appName := os.Getenv("APPNAME")
 	if appName == "" {
