78c3129e5da2517260eabbc67531e390c953017d
Satvik Vuppala <87299577+satvik-eltropy@users.noreply.github.com>
2022-08-03 13:57:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,40 +5,9 @@ import (
 	"helloworld/app"
 )
 
-//func NewString(s string) string {
-//	return s+"12345"
-//}
-
 func main() {
 
 	fmt.Println("Starting the application")
 
 	app.StartingFunction()
-	//r := gin.Default()
-
-	//host := os.Getenv("HOSTNAME")
-	//reply:= fmt.Sprintf("Hello from %s V7 \n", host)
-	//newStr := NewString("Hello, World! ")
-	//appName := os.Getenv("APPNAME")
-	//if appName == "" {
-	//	appName = "app"
-	//}
-	//
-	//r.GET("/hello", func(c *gin.Context) {
-	//	c.String(200, newStr)
-	//})
-    //
-	//r.GET("/"+appName+"/hello", func(c *gin.Context) {
-	//	c.String(200, reply)
-	//})
-	//
-	//r.GET("/"+appName+"/exit", func(c *gin.Context) {
-	//	os.Exit(0)
-	//})
-	//
-	//r.GET("/"+appName+"/health", func(c *gin.Context) {
-	//	c.String(200, "Success")
-	//})
-	//
-	//r.Run(":3000")
 }
